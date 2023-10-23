67248edb37b1d321295996879fd94532921c4612
Shantanu Mukherjee <shantanu.mukherjee@juspay.in>
2023-10-24 03:11:21 +0530

    added mongodb connection

--- Backend/src/server.ts
+++ Backend/src/server.ts
@@ -19,23 +19,3 @@ app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
 
-
-app.get('/post', async (req: any, res: any) => {
-  const access_token = "";
-  if (!access_token) {
-    return res.status(400).send('Access token not found.');
-  }
-
-  const mediaUrl = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSuyO9smXiWw5z6p0ve2QxRoSXbXgny_L9bh94BrKhbire2jW3Ckragd0aTMUXpWd9Pgxg&usqp=CAU';
-  const caption = 'Your caption here';
-
-  const postResponse = await axios.post('https://graph.facebook.com/v13.0/me/media', {
-    media_url: mediaUrl,
-    caption: caption,
-    access_token,
-  });
-
-  console.log(postResponse.data)
-
-  res.send('Post created successfully!');
-});
